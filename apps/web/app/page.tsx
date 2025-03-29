09727e2c6b4b12d10a6af33bf5f560f6b190f853
PraveenThanikachalam <praveenthanikachalam408@gmail.com>
2025-03-29 22:23:42 +0530

    Added Buttons 🔼

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -14,35 +14,47 @@ export default function Home() {
         objectFit="cover"
         priority
       />
-      <div className="flex w-full flex-col items-center justify-center overflow-hidden">
-        <div className="relative flex w-full -translate-y-[2vw] flex-col">
+      <div className="flex h-full w-full flex-col overflow-hidden md:justify-center">
+        <div className="relative flex w-full flex-col">
           <Link
             className={cn(
               TitleFont.className,
-              "flex items-center justify-center text-[24vw] text-[#FFD586] md:text-[14vw]",
+              "text-[24vw] text-[#FFD586] md:text-[14vw]",
             )}
             href="/"
           >
             DimAura
           </Link>
+
           <span
             className={cn(
               TitleFont.className,
-              "flex w-full -translate-y-[5vw] items-center justify-center pl-5 text-[6vw] text-white md:text-[3vw]",
-            )}
-          >
-            Break New Ground
-          </span>
-          <span
-            className={cn(
-              TitleFont.className,
-              "flex w-full -translate-y-[5vw] items-center justify-center pl-5 text-[6vw] text-[#FFD586] md:text-[3vw]",
+              "flex w-full -translate-y-[5vw] items-center pl-5 text-[6vw] text-[#FFD586] md:text-[3vw]",
             )}
           >
             AI + Content Creation = 🚀💪🏻{" "}
           </span>
+          <div className="mt-3 flex -translate-y-[6vw] flex-row gap-x-6 px-4">
+            <Link
+              className={cn(
+                TitleFont.className,
+                "glow-effect mt-4 flex w-full items-center justify-center rounded-3xl bg-gradient-to-r from-purple-950 to-indigo-950 p-2 pl-5 text-[6vw] text-white transition-all duration-300 ease-out hover:scale-[1.02] hover:shadow-lg hover:shadow-purple-600/50 md:w-fit md:px-8 md:text-[3vw]",
+              )}
+              href="/about"
+            >
+              <span className="relative">Dive InTo New World</span>
+            </Link>
+            <Link
+              className={cn(
+                TitleFont.className,
+                "mt-4 flex w-full items-center justify-center rounded-3xl p-2 pl-5 text-[6vw] text-[#FFD586] transition-all duration-300 ease-out hover:scale-[1.02] hover:shadow-lg hover:shadow-[#FFD586]/50 md:w-fit md:px-8 md:text-[2vw]",
+              )}
+              href="/dive-in"
+            >
+              <span className="relative">About Creators 🧑🏻‍💻</span>
+            </Link>
+          </div>
         </div>
-        <div className="flex w-full translate-y-10 items-start justify-end pr-4"></div>
       </div>
     </div>
   );
